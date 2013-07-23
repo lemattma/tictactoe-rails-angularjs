b1eedb24c71e0bba056a772637c7cc82edf5e0c4
Martin Miranda <martinmirandac@gmail.com>
2013-07-23 18:19:28 -0400

    Reload button, dependency injection

--- app/assets/javascripts/tic_tac_toe.js
+++ app/assets/javascripts/tic_tac_toe.js
@@ -12,24 +12,27 @@ angular.module('tictactoe', ['ngResource'])
   }
     
 }])
+
 // routes
-.config(function ($routeProvider, $locationProvider) {
+.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       controller: 'Dashboard',
       templateUrl: 'dashboard'
     });
-})
+    
+  $locationProvider.html5Mode(true);
+}])
 
 // Resource for server requests
-.factory('T3Resource', function ($resource) {
+.factory('T3Resource', ['$resource', function ($resource) {
   return $resource('/move.json', {}, {
     query: {method:'GET', params:{}, isArray:false}
   });
-})
+}])
 
 // Controllers
-.controller('Dashboard', function ($scope, $resource, T3Resource) {
+.controller('Dashboard', ['$scope', '$resource', 'T3Resource', '$route', function ($scope, $resource, T3Resource, $route) {
   
   $scope.available_cells = [1,2,3,4,5,6,7,8,9];
   
@@ -78,11 +81,13 @@ angular.module('tictactoe', ['ngResource'])
   $scope.win = function(){
     winner = false;
     
+    // check winner for x and o
     $.each(['xsign','osign'], function(i,v) {
       
       name = (v == 'xsign') ? 'X' : 'O';
       winning_formulas = ['#1, #2, #3','#4, #5, #6','#7, #8, #9','#3, #6, #9','#1, #4, #7','#1, #5, #9','#3, #5, #7'];
       
+      // check each winning row possible for x and o
       $.each(winning_formulas, function(id,vl) {
         if ($(vl).find('.'+v).length==3) { 
           winner = name; 
@@ -96,16 +101,20 @@ angular.module('tictactoe', ['ngResource'])
   }
   
   $scope.show = function(event){
-    if ($scope.winner) return false;
+    if ($scope.winner) return false; // If winner, why continue?
     
     $(event.target).find('.btn').fadeIn(100);
   };
   
   $scope.hide = function(event){
-    if ($scope.winner) return false;
+    if ($scope.winner) return false; // If winner, why continue?
     
     $(event.target).find('.btn').fadeOut(200);
   };
+  
+  $scope.playAgain = function(){
+    $route.reload();
+  }
 
   
-});
+}]);
